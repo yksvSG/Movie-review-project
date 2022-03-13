7450ad6efaa99387d8db78fd0e94b26a8026a4a1
yksvSG <sg.yksv77@gmail.com>
2022-03-13 16:58:33 +0900

    sort 정렬 변경-최신순, 평점 버튼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleBestClick}>평점</button>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
